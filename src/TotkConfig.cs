6e0283a4ae47eae21f7c42b4e35ed53a9949d3ae
ArchLeaders <archparliament@gmail.com>
2023-05-01 21:54:05 -0700

    Add TotkConfig

---
+++ src/TotkConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace Totk.ZStdTool;
+
+public class TotkConfig
+{
+    private static readonly string _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Totk", "config.json");
+
+    public required string GamePath { get; set; }
+
+    public static TotkConfig Load()
+    {
+        using FileStream fs = File.OpenRead(_path);
+        return JsonSerializer.Deserialize<TotkConfig>(fs) ?? Create();
+    }
+
+    public void Save()
+    {
+        FileStream fs = File.Create(_path);
+        JsonSerializer.Serialize(fs, this);
+    }
+
+    private static TotkConfig Create()
+    {
+        TotkConfig config = new() {
+            GamePath = string.Empty
+        };
+
+        config.Save();
+        return config;
+    }
+}
