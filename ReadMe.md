c6f21d4611c48eccd82bf1c89609dcb47cf9a964
Marcus André <ddtank.marcus@gmail.com>
2023-05-04 13:22:00 -0300

    Add CLI

--- ReadMe.md
+++ ReadMe.md
@@ -19,6 +19,13 @@ A simple tool to decompress `.zs` files in Totk, compression support is also pla
 - Browse for a `.zs` file in your Totk game dump
 - Click `Decompress` and save the raw file to your computer
 
+## Usage (CLI)
+- For CLI works, you must setup the game path before.
+- Decompress `Totk.ZStdTool.exe -x Something.zs`
+  - The output will be in the same folder but without `.zs` extension
+- Compress `Totk.ZStdTool.exe -c Something.bin`
+  - The output will be in the same folder but with `.zs.new` extension
+
 <br>
 
 *(c) Arch Leaders - 2023*
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using Cead.Interop;
+using Totk.ZStdTool.Helpers;
 
 namespace Totk.ZStdTool;
 
@@ -9,8 +11,62 @@ internal class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        if (args?.Length > 0)
+        {
+            CLI(args);
+        }
+        else
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+    }
+
+    private static void CLI(string[] Args)
+    {
+        DllManager.LoadCead();
+
+        bool CompressMode = false;
+
+        while (Args.Any())
+        {
+            var Entry = Args.First();
+            Args = Args.Skip(1).ToArray();
+
+            bool IsFlag = Entry.StartsWith('-') || Entry.StartsWith('/');
+            if (IsFlag)
+            {
+                var Flag = Entry.Substring(1).ToLower();
+                switch (Flag)
+                {
+                    case "x":
+                        CompressMode = false;
+                        break;
+                    case "c":
+                        CompressMode = true;
+                        break;
+                }
+                continue;
+            }
+
+            if (File.Exists(Entry))
+            {
+                if (CompressMode)
+                {
+                    string OutPath = Entry + ".zs.new";
+                    var Data = ZStdHelper.Compress(Entry);
+                    File.WriteAllBytes(OutPath, Data.ToArray());
+                }
+                else
+                {
+                    string OutPath = Path.Combine(Path.GetDirectoryName(Entry), Path.GetFileNameWithoutExtension(Entry));
+                    var Data = ZStdHelper.Decompress(Entry);
+                    File.WriteAllBytes(OutPath, Data.ToArray());
+                }
+            }
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
